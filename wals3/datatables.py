dcccd9cee46c23366733a6645c3f3fae2460bc39
Melvin <melvinsamson.steiger@uzh.ch>
2022-06-27 17:17:46 +0200

    changing names of datatables

--- wals3/datatables.py
+++ wals3/datatables.py
@@ -236,8 +236,9 @@ class Chapters(datatables.Contributions):
 
 
 def includeme(config):
+    # the name of the datatable must be the same as the name given to the route pattern
     config.register_datatable('contributions', Chapters)
     config.register_datatable('units', Units)
-    config.register_datatable('ooalanguages', Languages)
+    config.register_datatable('languages', Languages)
     config.register_datatable('parameters', Features)
-    config.register_datatable('ooafeaturesets', Featuresets)
+    config.register_datatable('featuresets', Featuresets)
