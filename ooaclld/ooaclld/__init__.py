831b91b2e714175bb17886ea7e321baa71a21c90
Melvin <melvinsamson.steiger@uzh.ch>
2022-07-07 14:58:38 +0200

    clld from scratch

--- ooaclld/ooaclld/__init__.py
+++ ooaclld/ooaclld/__init__.py
@@ -11,6 +11,40 @@ from clldutils.svg import pie, icon, data_url
 from ooaclld import models
 
 
+def map_marker(ctx, req):
+    """allow for user-selectable markers.
+
+    we have to look up a possible custom selection from the url params.
+    """
+    icon = None
+
+    if IValue.providedBy(ctx):
+        icon = req.params.get(
+            'v%s' % ctx.domainelement.number,
+            ctx.domainelement.jsondata['icon'])
+    elif IDomainElement.providedBy(ctx):
+        icon = req.params.get('v%s' % ctx.number, ctx.jsondata['icon'])
+    elif ILanguage.providedBy(ctx):
+        # hard coded icon
+        icon = req.params.get(ctx.id, 'c0000dd')
+        #icon = req.params.get(ctx.id, ctx.genus.icon)
+    elif isinstance(ctx, Genus):
+        icon = req.params.get(ctx.id, ctx.icon)
+
+    if icon:
+        if "'" in icon:
+            icon = icon.split("'")[0]
+        if len(icon) > 4 and len(icon) != 7:
+            icon = icon[:4]
+        if len(icon) == 4:
+            icon = icon[0] + 2*icon[1] + 2*icon[2] + 2*icon[3]
+        if icon.startswith('a'):
+            return svg.data_url(svg.icon('c000000', opacity='0'))
+        try:
+            return svg.data_url(svg.icon(icon))
+        except KeyError:
+            return ''
+
 
 class LanguageByFamilyMapMarker(util.LanguageByFamilyMapMarker):
     def __call__(self, ctx, req):
@@ -22,6 +56,20 @@ class LanguageByFamilyMapMarker(util.LanguageByFamilyMapMarker):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['route_patterns'] = {
+        'languages': r'/ooalanguages',
+        'language': r'/ooalanguages/{id:[^/\.]+}',
+        'source': r'/refdb/record/{id:[^/\.]+}',
+        'sources': '/refdb',
+        'parameters': '/ooafeatures',
+        'parameter': r'/ooafeatures/{id:[^/\.]+}',
+        'featuresets': r'/ooafeaturesets',
+        'featureset': r'/ooafeaturesets/{id:[^/\.]+}',
+        'codes': r'/domainelement',
+        'code': r'/domainelement/{id}',
+        'units': r'/ooaunits',
+        'unit': r'/ooaunits/{id}'
+    }
     config = Configurator(settings=settings)
     config.include('clld.web.app')
 
